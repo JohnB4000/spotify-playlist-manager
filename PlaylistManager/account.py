928eb83a7883272b9af2399555fcffb26bde4052
JohnB4000 <jb2069@hw.ac.uk>
2023-10-20 21:39:16 +0100

    Start implementing account and restructure static

--- PlaylistManager/account.py
+++ PlaylistManager/account.py
@@ -6,7 +6,17 @@ from .auth import login_required
 
 bp = Blueprint('account', __name__, url_prefix='/account')
 
-@bp.route('/', methods=('GET', 'POST'))
+@bp.route('/')
 @login_required
 def account():
 	return render_template("account/account.html")
+
+@bp.route('/')
+@login_required
+def changeUsername():
+	return render_template("account/changeUsername.html")
+
+@bp.route('/')
+@login_required
+def changePassword():
+	return render_template("account/changePassword.html")
